744b44210b14dedf21ed6179f03b08554912bcf7
Kobie127 <arndtkobie@gmail.com>
2020-05-12 13:16:45 -0400

    React router package added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage-component';
+
+
 
 function App() {
   return (
