79e028aa2ab05e1b4b6cc2a5fe1827366f4a23ac
Kobie127 <arndtkobie@gmail.com>
2020-05-12 14:20:37 -0400

    New shop page component added.  All shopping items are stored as SHOP_DATA from another file for now.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage-component';
-
-const HatsPage = () => (
-  <h2>HATS PAGE</h2>
-);
+import ShopPage from './pages/shop/shop.component';
 
 
 function App() {
@@ -15,7 +12,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path ='/' component = {HomePage}/>
-        <Route exact path = '/hats' component = {HatsPage}/>
+        <Route exact path ='/shop' component = {ShopPage}/>
       </Switch>
     </div>
       
