e6200be2a16bc55549472e54ab3f67a0d4c2a30a
Kobie127 <arndtkobie@gmail.com>
2020-05-12 18:57:58 -0400

    Custom button for reuse implemented

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import FormInput from '../../components/form-input/form-input.component';
-
+import CustomButtoin from '../../components/custom-button/custom-button.component';
 import './sign-in.styles.scss';
 import { Component } from 'react';
+import CustomButton from '../../components/custom-button/custom-button.component';
 
 class SignIn extends Component{
     constructor(props){
@@ -33,7 +34,7 @@ class SignIn extends Component{
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name='email' type = 'email' value={this.state.email}  label = "email" handleChange={this.handleChange} required/>
                     <FormInput name='password' type = 'password' value ={this.state.password} label="password" handleChange={this.handleChange} required/>
-                    <input type ='submit' value='Submit Form'/>
+                    <CustomButton type ='submit'>Sign in</CustomButton>
                 </form>
             </div>
         )
