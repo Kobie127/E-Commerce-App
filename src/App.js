a7d9ee759d0648ab6638f48b8982d751b92b7724
Kobie127 <arndtkobie@gmail.com>
2020-05-18 16:15:41 -0400

    Auth with firebase implemented, button for signing in with google functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
@@ -7,20 +7,44 @@ import HomePage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import{auth} from './firebase/firebase.utils';
 
+class App extends Component {
+  constructor(){
+    super();
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path ='/' component = {HomePage}/>
-        <Route exact path ='/shop' component = {ShopPage}/>
-        <Route exact oath ='/signin' component = {SignInAndSignUpPage}/>
-      </Switch>
-    </div>
-      
-  );
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubcribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubcribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header currentUser = {this.state.currentUser}/>
+        <Switch>
+          <Route exact path ='/' component = {HomePage}/>
+          <Route exact path ='/shop' component = {ShopPage}/>
+          <Route exact oath ='/signin' component = {SignInAndSignUpPage}/>
+        </Switch>
+      </div>
+        
+    );
+  }
+ 
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -35,8 +35,10 @@ class SignIn extends Component{
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name='email' type = 'email' value={this.state.email}  label = "email" handleChange={this.handleChange} required/>
                     <FormInput name='password' type = 'password' value ={this.state.password} label="password" handleChange={this.handleChange} required/>
-                    <CustomButton type ='submit'>Sign in</CustomButton>
-                    <CustomButton onClick = {signInWithGoogle}>{' '}Sign in with Google {' '}</CustomButton>
+                    <div className ='buttons'>
+                        <CustomButton type ='submit'>Sign in</CustomButton>
+                        <CustomButton onClick = {signInWithGoogle} isGoogleSignIn>{' '}Sign in with Google {' '}</CustomButton>
+                    </div>  
                 </form>
             </div>
         )
